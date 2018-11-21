42b10b449e51a87c4e60ff1a62fe4c860da5c0e5
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-20 20:59:58 -0600

    edit functionality working great

--- src/components/AddEditFriends/addEditFriends.js
+++ src/components/AddEditFriends/addEditFriends.js
@@ -3,27 +3,27 @@ import authHelpers from '../../helpers/authHelpers';
 import friendsData from '../../helpers/data/friendsData';
 import initializeFriendsPage from '../FriendsPage/friendsPage';
 
-const formBuilder = () => {
+const formBuilder = (friend) => {
   const form = `
   <div class="form-group">
     <label for="form-friend-name">Name: </label>
-    <input type="text" class="form-control" id="form-friend-name" placeholder="John Smith">
+    <input type="text" class="form-control" value="${friend.name}" id="form-friend-name" placeholder="John Smith">
   </div>
   <div class="form-group">
     <label for="form-friend-address">Address: </label>
-    <input type="text" class="form-control" id="form-friend-address" placeholder="123 Blvd">
+    <input type="text" class="form-control" value="${friend.address}" id="form-friend-address" placeholder="123 Blvd">
   </div>
   <div class="form-group">
     <label for="form-friend-email">Email: </label>
-    <input type="text" class="form-control" id="form-friend-email" placeholder="johnny@schittscreek.com">
+    <input type="text" class="form-control" value="${friend.email}" id="form-friend-email" placeholder="johnny@schittscreek.com">
   </div>
   <div class="form-group">
     <label for="form-friend-phone">Phone Number: </label>
-    <input type="text" class="form-control" id="form-friend-phone" placeholder="888-888-8888">
+    <input type="text" class="form-control" value="${friend.phoneNumber}" id="form-friend-phone" placeholder="888-888-8888">
   </div>
   <div class="form-group">
     <label for="form-friend-relationship">Relationship: </label>
-    <input type="text" class="form-control" id="form-friend-relationship" placeholder="3rd Cousin">
+    <input type="text" class="form-control" value="${friend.relationship}" id="form-friend-relationship" placeholder="3rd Cousin">
   </div>
   `;
   return form;
@@ -43,8 +43,15 @@ const gettingFriendFromForm = () => {
 };
 
 const buildAddForm = () => {
+  const emptyFriend = {
+    name: '',
+    address: '',
+    email: '',
+    phoneNumber: '',
+    relationship: '',
+  };
   let domString = '<h2>Add New Friend</h2>';
-  domString += formBuilder();
+  domString += formBuilder(emptyFriend);
   domString += '<button id="add-friend">Save New Friend</button>';
   $('#add-edit-friend').html(domString).show();
   $('#friends').hide();
@@ -56,13 +63,46 @@ const plusNewFriend = () => {
     .then(() => {
       $('#add-edit-friend').html('').hide();
       $('#friends').show();
-      initializeFriendsPage();
+      initializeFriendsPage(); // always need to call the database like this
     })
     .catch((error) => {
       console.error('error', error);
     });
 };
 
-$('body').on('click', '#add-friend', plusNewFriend);
+// Edit
+const showEditForm = (e) => {
+  const idToEdit = e.target.dataset.editId;
+  friendsData.getSingleFriend(idToEdit)
+    .then((singleFriend) => {
+      let domString = '<h2>Edit Friend</h2>';
+      domString += formBuilder(singleFriend);
+      domString += `<button id="edit-friend" data-single-edit-id=${singleFriend.id}>Save Friend</button>`;
+      $('#add-edit-friend').html(domString).show();
+      $('#friends').hide();
+    })
+    .catch((error) => {
+      console.error('error in getting single friend', error);
+    });
+};
+
+const updateFriend = (e) => {
+  const updatedFriend = gettingFriendFromForm();
+  const friendId = e.target.dataset.singleEditId;
+  friendsData.updateFriend(updatedFriend, friendId)
+    .then(() => {
+      $('#add-edit-friend').html('').hide();
+      $('#single-container').html('');
+      $('#friends').show();
+      initializeFriendsPage(); // always need to call the database like this
+    })
+    .catch((error) => {
+      console.error('error', error);
+    });
+};
+
+$('body').on('click', '#add-friend', plusNewFriend); // Adds friend
+$('body').on('click', '.edit-btn', showEditForm); // Shows Edit form
+$('body').on('click', '#edit-friend', updateFriend); // Saves Edit
 
 export default buildAddForm;
