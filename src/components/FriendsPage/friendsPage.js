36b1dfaf5165104c2e7e3305d6977c8f036efd7e
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-20 19:11:19 -0600

    everything switched over

--- src/components/FriendsPage/friendsPage.js
+++ src/components/FriendsPage/friendsPage.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import $ from 'jquery';
-import apiKeys from '../../../db/apiKeys.json';
 import authHelpers from '../../helpers/authHelpers';
 import friendsData from '../../helpers/data/friendsData';
 
@@ -49,16 +47,8 @@ const buildDropdown = (friendsArray) => {
 
 const friendsPage = () => {
   const uid = authHelpers.getCurrentUid();
-  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends.json?orderBy="uid"&equalTo="${uid}"`)
-    .then((results) => {
-      const friendsObject = results.data;
-      const friendsArray = [];
-      if (friendsObject !== null) {
-        Object.keys(friendsObject).forEach((friendId) => {
-          friendsObject[friendId].id = friendId; // assigning a new id to the friend object
-          friendsArray.push(friendsObject[friendId]);
-        });
-      }
+  friendsData.getAllFriends(uid)
+    .then((friendsArray) => {
       buildDropdown(friendsArray);
     })
     .catch((error) => {
@@ -69,7 +59,7 @@ const friendsPage = () => {
 const deleteFriend = (e) => {
   // firebase id
   const idToDelete = e.target.dataset.deleteId;
-  axios.delete(`${apiKeys.firebaseKeys.databaseURL}/friends/${idToDelete}.json`)
+  friendsData.deleteFriend(idToDelete)
     .then(() => {
       friendsPage();
       $('#single-container').html('');
